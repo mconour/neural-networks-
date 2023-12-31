6e621838fa0bcf86d8ba0f2a635adfc0ba6490c8
mconour <michaelconour@gmail.com>
2023-12-30 21:34:53 -0800

    implement assignOwnership and viewAsset functions

--- contracts/AssetTokenization.sol
+++ contracts/AssetTokenization.sol
@@ -14,15 +14,19 @@ contract AssetTokenization {
     mapping(uint256 => Asset) public assets;
 
     function createAsset(string memory name, string memory description, uint256 value) public {
-        // Increment the assetCounter to get a new ID
-        assetCounter++;
+        assetCounter++; // Increment the asset counter to get a new ID
+        address assetOwner = msg.sender; // Owner of the asset is the sender of the transaction
+        assets[assetCounter] = Asset(assetCounter, name, description, value, assetOwner); // Store the new asset
+    }
 
-        // Set the owner of the asset to the sender of the transaction
-        address assetOwner = msg.sender;
+    function assignOwnership(uint256 assetId, address newOwner) public {
+        require(assets[assetId].id != 0, "Asset does not exist.");
+        require(assets[assetId].owner == msg.sender, "Caller is not the owner.");
+        assets[assetId].owner = newOwner; // Update the owner of the asset
+    }
 
-        // Create a new asset and store it in the mapping
-        assets[assetCounter] = Asset(assetCounter, name, description, value, assetOwner);
-
-        // Emit an event here if you want to notify front ends of the asset creation
+    function viewAsset(uint256 assetId) public view returns (Asset memory) {
+        require(assets[assetId].id != 0, "Asset does not exist.");
+        return assets[assetId]; // Return the asset details
     }
 }
